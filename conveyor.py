9e0ba5c97dd804c85c6e99b66b20e456bcd4d560
Mao <maoshanshi@bruce.staff.vuw.ac.nz>
2026-04-27 12:08:00 +1200

    format and igore vscode

--- conveyor.py
+++ conveyor.py
@@ -1,14 +1,15 @@
 import numpy as np
 from split_gen import *
 
+
 class ItemGenerator:
     def __init__(self, k=1):
-        assert 0 < k, f'invalid argument k={k}'
+        assert 0 < k, f"invalid argument k={k}"
         self.k = k
-        
-    def reset(self): 
-        raise Exception('not implemened')
-        
+
+    def reset(self):
+        raise Exception("not implemened")
+
     def peek(self):
         # lookahead incoming (k) items
         items = self._items
@@ -19,28 +20,29 @@ class ItemGenerator:
                 item = None
             items.append(item)
         return items
-        
+
     def grab(self, n=0):
-        assert 0 <= n < self.k, f'invalid argument n={n}'
+        assert 0 <= n < self.k, f"invalid argument n={n}"
         return self.peek().pop(n)
 
+
 class FileConveyor(ItemGenerator):
-    def __init__(self, k=1, path='./input.txt'):
+    def __init__(self, k=1, path="./input.txt"):
         super().__init__(k)
 
         self.path = path
-        
+
         self._items = None
         self._item_iter = None
 
         self.loaded = False
-        
+
     def _iter(self):
-        with open(self.path, 'r') as file:
+        with open(self.path, "r") as file:
             for line in file.readlines():
-                w, h, d = map(int, line.split(' '))
+                w, h, d = map(int, line.split(" "))
                 yield w, h, d
-        
+
     def reset(self):
         if not self.loaded:
             self.loaded = True
@@ -48,31 +50,34 @@ class FileConveyor(ItemGenerator):
             self._items = []
             self._item_iter = self._iter()
         return self
+
 
 class InputConveyor(ItemGenerator):
     def __init__(self, k=1):
         super().__init__(k)
-        
+
         self._items = None
         self._item_iter = None
 
         self.loaded = False
-        
+
     def _iter(self):
         while True:
             try:
-                input_str = input('item: ')
-                if input_str == '-1':
+                input_str = input("item: ")
+                if input_str == "-1":
                     break
-                w, h, d = map(int, input_str.split(' '))
+                w, h, d = map(int, input_str.split(" "))
                 yield (w, h, d)
             except KeyboardInterrupt:
                 break
             except:
-                print(f'invalid format. each line must contain [w: int] [h: int] [d: int]')
-                print(f'for example: 4 5 6')
+                print(
+                    f"invalid format. each line must contain [w: int] [h: int] [d: int]"
+                )
+                print(f"for example: 4 5 6")
                 continue
-        
+
     def reset(self):
         if not self.loaded:
             self.loaded = True
@@ -81,48 +86,65 @@ class InputConveyor(ItemGenerator):
             self._item_iter = self._iter()
         return self
 
+
 class Conveyor(ItemGenerator):
-    def __init__(self, k=1, prealloc_bins=0, prealloc_items=0, max_items=None, max_spaces=None, size=(32, 32, 32), lb=(6, 6, 6), ub=(12, 12, 12), p=0.0, p_decay=1.0, shuffle=False, assigned_items=None):
+    def __init__(
+        self,
+        k=1,
+        prealloc_bins=0,
+        prealloc_items=0,
+        max_items=None,
+        max_spaces=None,
+        size=(32, 32, 32),
+        lb=(6, 6, 6),
+        ub=(12, 12, 12),
+        p=0.0,
+        p_decay=1.0,
+        shuffle=False,
+        assigned_items=None,
+    ):
         super().__init__(k)
-        
+
         self._items = None
         self._item_iter = None
-        
+
         self.max_items = max_items
         self.max_spaces = max_spaces
-        
+
         if self.max_spaces is not None and self.prealloc_bins > self.max_spaces:
-            raise Exception('self.prealloc_bins > max_spaces')
-        
+            raise Exception("self.prealloc_bins > max_spaces")
+
         self.prealloc_bins = prealloc_bins
         self.prealloc_items = prealloc_items
-        self.buffer = None
-        
+        self.buffer = []
+
         self.assigned_items = assigned_items
-        
-        self.split_generator = lambda: nongullotine_cut(size, lb, ub, p, p_decay, shuffle=shuffle)
-        
+
+        self.split_generator = lambda: nongullotine_cut(
+            size, lb, ub, p, p_decay, shuffle=shuffle
+        )
+
     def _iter(self):
         if self.assigned_items is not None:
             for item in (np.asarray(item) for item in self.assigned_items):
                 yield item
             return
-        
+
         n_items = 0
         n_spaces = 0
-        
+
         for split in self.buffer:
             n_items += 1
             if self.max_items is not None and n_items > self.max_items:
                 return
             yield np.asarray(split.size)
         n_spaces += self.prealloc_bins
-        
+
         while True:
             n_spaces += 1
             if self.max_spaces is not None and n_spaces > self.max_spaces:
                 return
-                
+
             splits = self.split_generator()
             for split in splits:
                 n_items += 1
@@ -132,39 +154,40 @@ class Conveyor(ItemGenerator):
 
     def dump(self, n_items, path):
         self.reset()
-        file = open(path, 'w')
+        file = open(path, "w")
         for _ in range(n_items):
-            print(' '.join(map(str, self.grab())), file=file)
+            print(" ".join(map(str, self.grab())), file=file)
         file.close()
-        
+
     def reset(self):
-        self.buffer = [] # consistent rng
+        self.buffer = []  # consistent rng
         for i in range(self.prealloc_bins):
             self.buffer.extend(self.split_generator())
-        
+
         while len(self.buffer) < self.prealloc_items:
             self.buffer.extend(self.split_generator())
-            
+
         self._items = []
         self._item_iter = self._iter()
         return self
-    
+
+
 def rotated_sizes(item, rotate=True, remove_duplicate=True):
     if rotate is True:
-        rotate = 'xyz'
+        rotate = "xyz"
     elif rotate is False:
-        rotate = ''
-    
+        rotate = ""
+
     w, h, d = item
     sizes = [(w, h, d)]
-    if 'x' in rotate:
+    if "x" in rotate:
         sizes.extend((w, d, h) for w, h, d in sizes[:])
-    if 'y' in rotate:
+    if "y" in rotate:
         sizes.extend((d, h, w) for w, h, d in sizes[:])
-    if 'z' in rotate:
+    if "z" in rotate:
         sizes.extend((h, w, d) for w, h, d in sizes[:])
-            
+
     if remove_duplicate:
         sizes = list(set(sizes))
-        
+
     return sizes
--- deeppack3d.py
+++ deeppack3d.py
@@ -1,167 +1,262 @@
+import numpy as np
+import os
+import shutil
+import time
+
+# import seaborn as sns
+import matplotlib.pyplot as plt
+
+from env import MultiBinPackerEnv
+from conveyor import FileConveyor, InputConveyor
+from agent import (
+    bottom_left,
+    best_area_fit,
+    best_short_side_fit,
+    best_long_side_fit,
+    Agent,
+    HeuristicAgent,
+)
+from split_gen import reset_rng
+
+
 def parse_args():
     import argparse
-    
+
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('method', metavar='method', 
-                        type=str, choices=['rl', 'bl', 'baf', 'bssf', 'blsf'], 
-                        help='choose the method from {"rl", "bl", "baf", "bssf", "blsf"}.')
-    
-    parser.add_argument('lookahead', metavar='lookahead', 
-                        type=int,
-                        help='choose the lookahead value.')
-    
-    parser.add_argument('--data', metavar='', 
-                        type=str, default='generated', choices=['generated', 'input', 'file'], 
-                        help='choose the input source from {"generated", "input", "file"} (default: generated).')
-    
-    parser.add_argument('--path', metavar='', 
-                        type=str, default=None, 
-                        help='set the file path, only used if --data is "file" (default: None).')
-    
-    parser.add_argument('--n_iterations', metavar='', 
-                        type=int, default=100, 
-                        help='set the number of iterations, only used if --data is "generated" (default: 100).')
-    
-    parser.add_argument('--seed', metavar='', 
-                        type=str, default=None, 
-                        help='set the random seed for reproducibility, only used if --data is "generated" (default: None).')
-    
-    parser.add_argument('--verbose', metavar='', 
-                        type=int, default=1, 
-                        help='set verbose level (default: 1).')
-    
-    parser.add_argument('--train', 
-                        action='store_true', 
-                        help='enable training mode, only used if method is "rl" (default: False).')
-    
-    parser.add_argument('--batch_size', metavar='', 
-                        type=int, default=32, 
-                        help='set batch_size, only used if train is True (default: 32).')
-    
-    parser.add_argument('--visualize', 
-                        action='store_true', 
-                        help='enable visualization mode (default: False).')
-    
+
+    parser.add_argument(
+        "method",
+        metavar="method",
+        type=str,
+        choices=["rl", "bl", "baf", "bssf", "blsf"],
+        help='choose the method from {"rl", "bl", "baf", "bssf", "blsf"}.',
+    )
+
+    parser.add_argument(
+        "lookahead", metavar="lookahead", type=int, help="choose the lookahead value."
+    )
+
+    parser.add_argument(
+        "--data",
+        metavar="",
+        type=str,
+        default="generated",
+        choices=["generated", "input", "file"],
+        help='choose the input source from {"generated", "input", "file"} (default: generated).',
+    )
+
+    parser.add_argument(
+        "--path",
+        metavar="",
+        type=str,
+        default=None,
+        help='set the file path, only used if --data is "file" (default: None).',
+    )
+
+    parser.add_argument(
+        "--n_iterations",
+        metavar="",
+        type=int,
+        default=100,
+        help='set the number of iterations, only used if --data is "generated" (default: 100).',
+    )
+
+    parser.add_argument(
+        "--seed",
+        metavar="",
+        type=str,
+        default=None,
+        help='set the random seed for reproducibility, only used if --data is "generated" (default: None).',
+    )
+
+    parser.add_argument(
+        "--verbose",
+        metavar="",
+        type=int,
+        default=1,
+        help="set verbose level (default: 1).",
+    )
+
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help='enable training mode, only used if method is "rl" (default: False).',
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        metavar="",
+        type=int,
+        default=32,
+        help="set batch_size, only used if train is True (default: 32).",
+    )
+
+    parser.add_argument(
+        "--visualize",
+        action="store_true",
+        help="enable visualization mode (default: False).",
+    )
+
     return parser.parse_args()
 
-import numpy as np
-import os, shutil, time
-
-from env import *
-from agent import *
 
 heuristics = {
-    'bl': bottom_left,
-    'baf': best_area_fit, 
-    'bssf': best_short_side_fit, 
-    'blsf': best_long_side_fit, 
+    "bl": bottom_left,
+    "baf": best_area_fit,
+    "bssf": best_short_side_fit,
+    "blsf": best_long_side_fit,
 }
 
-def deeppack3d(method, lookahead, *, n_iterations=100, seed=None, verbose=1, data='generated', path=None, train=False, visualize=False, batch_size=32):
-    reset_rng(seed)
-    
-    env = MultiBinPackerEnv(n_bins=1, 
-                            max_bins=1, 
-                            size=(32, 32, 32), 
-                            k=lookahead, 
-                            prealloc_items=100, 
-                            verbose=verbose)
 
-    if data == 'file':
+def deeppack3d(
+    method,
+    lookahead,
+    *,
+    n_iterations=100,
+    seed=None,
+    verbose=1,
+    data="generated",
+    path="",
+    train=False,
+    visualize=False,
+    batch_size=32,
+):
+    reset_rng(seed)
+
+    env = MultiBinPackerEnv(
+        n_bins=1,
+        max_bins=1,
+        size=(32, 32, 32),
+        k=lookahead,
+        prealloc_items=100,
+        verbose=verbose,
+    )
+
+    if data == "file":
         env.conveyor = FileConveyor(k=env.k, path=path).reset()
-    elif data == 'input':
+    elif data == "input":
         env.conveyor = InputConveyor(k=env.k).reset()
 
     if visualize:
-        if os.path.exists('./outputs'):
-            shutil.rmtree('./outputs')
-        os.makedirs('./outputs')
+        if os.path.exists("./outputs"):
+            shutil.rmtree("./outputs")
+        os.makedirs("./outputs")
 
     if train:
         print(f'Training with method "{method}" and lookahead {lookahead}...')
-        
-        if method != 'rl':
+
+        if method != "rl":
             raise Exception('training mode can only be used if method is "rl"')
 
         # env = BinPackerEnv(size=(32, 32, 32), k=env.k, bin_size=(32, 32, 32))
-        agent = Agent(env, train=True, verbose=verbose > 0, visualize=visualize, batch_size=batch_size)
+        agent = Agent(
+            env,
+            train=True,
+            verbose=verbose > 0,
+            visualize=visualize,
+            batch_size=batch_size,
+        )
 
         agent.eps = 1.0
         for i in range(n_iterations):
-            print(f'Iteration {i}')
+            print(f"Iteration {i}")
             start_time = time.time()
             yield from agent.run(100, verbose=verbose > 1)
             agent.eps = max(agent.eps * 0.95, 0.025)
-            
+
         data = np.asarray([utils for utils, n_bins, ep_reward in agent.ep_history])
         # y = np.ones(100)
         # data = np.convolve(data, y, 'valid') / len(y)
         sns.lineplot(data=data)
-        plt.savefig(f'./util.jpg')
+        plt.savefig("./util.jpg")
         plt.show()
-        
+
         data = np.asarray([ep_reward for utils, n_bins, ep_reward in agent.ep_history])
         # y = np.ones(100)
         # data = np.convolve(data, y, 'valid') / len(y)
         sns.lineplot(data=data)
-        plt.savefig(f'./ep_reward.jpg')
+        plt.savefig("./ep_reward.jpg")
         plt.show()
 
         import uuid
+
         uid = uuid.uuid4()
-        print(f'saved model at ./{uid}.h5')
-        agent.q_net.save(f'{uid}.h5')
+        print(f"saved model at ./{uid}.h5")
+        agent.q_net.save(f"{uid}.h5")
     else:
         if verbose > 0:
             print(f'Testing with method "{method}" and lookahead {lookahead}...')
-        
-        if method == 'rl':
-            model_path = f'./models/k={lookahead}.h5'
-            agent = Agent(env, train=False, verbose=verbose > 0, visualize=visualize, batch_size=batch_size)
+
+        if method == "rl":
+            model_path = f"./models/k={lookahead}.h5"
+            agent = Agent(
+                env,
+                train=False,
+                verbose=verbose > 0,
+                visualize=visualize,
+                batch_size=batch_size,
+            )
             agent.q_net = tf.keras.models.load_model(model_path, compile=False)
             agent.eps = 0.0
         else:
-            agent = HeuristicAgent(heuristics[method], env, verbose=verbose > 0, visualize=visualize)
-        
+            agent = HeuristicAgent(
+                heuristics[method], env, verbose=verbose > 0, visualize=visualize
+            )
+
         start_time = time.time()
-        
+
         try:
             yield from agent.run(n_iterations, verbose=verbose > 1)
         except Exception as e:
             if np.all(np.array(env.conveyor.reset().peek()) == None):
                 if verbose > 0:
-                    print('\n=====the end of conveyor line=====')
+                    print("\n=====the end of conveyor line=====")
             else:
                 print(e)
 
         if verbose > 0:
             print()
             next_items = np.array(env.conveyor.reset().peek()).tolist()
-            avg_util = np.mean([util for utils, n_bins, ep_reward in agent.ep_history[:] for util in utils[:]])
-            used_items = np.sum([n_bins for utils, n_bins, ep_reward in agent.ep_history[:] for util in utils[:]])
-            
-            print(f'Used time: {int(time.time() - start_time)} seconds')
-            print(f'Next items: {next_items}')
-            print(f'Average space util: {avg_util}')
-            print(f'Used bins: {used_items}')
+            avg_util = np.mean(
+                [
+                    util
+                    for utils, n_bins, ep_reward in agent.ep_history[:]
+                    for util in utils[:]
+                ]
+            )
+            used_items = np.sum(
+                [
+                    n_bins
+                    for utils, n_bins, ep_reward in agent.ep_history[:]
+                    for util in utils[:]
+                ]
+            )
+
+            print(f"Used time: {int(time.time() - start_time)} seconds")
+            print(f"Next items: {next_items}")
+            print(f"Average space util: {avg_util}")
+            print(f"Used bins: {used_items}")
+
 
 def main():
     args = parse_args()
-    
+
     reset_rng(args.seed)
 
-    for _ in deeppack3d(args.method, 
-                        args.lookahead, 
-                        n_iterations=args.n_iterations, 
-                        seed=args.seed, 
-                        train=args.train, 
-                        verbose=args.verbose, 
-                        data=args.data, 
-                        path=args.path,
-                        visualize=args.visualize, 
-                        batch_size=args.batch_size):
+    for _ in deeppack3d(
+        args.method,
+        args.lookahead,
+        n_iterations=args.n_iterations,
+        seed=args.seed,
+        train=args.train,
+        verbose=args.verbose,
+        data=args.data,
+        path=args.path,
+        visualize=args.visualize,
+        batch_size=args.batch_size,
+    ):
         pass
+
 
 if __name__ == "__main__":
     main()

9a61d4f1082860bcfd242cb4b548795991e7a612
Mao <maoshanshi@outlook.com>
2026-05-09 16:22:40 +1200

    remove trivial code

--- gp/evaluate.py
+++ gp/evaluate.py
@@ -1,11 +1,7 @@
 import itertools
 import copy
-import numpy as np
 
-from concurrent.futures import ThreadPoolExecutor
 from .util.sequencing import GP_action_selector
-
-from rl.geometry import Cuboid
 
 
 def _evaluate_individual(agent, individual):
@@ -19,8 +15,6 @@ def _evaluate_individual(agent, individual):
 
         if done:
             break
-        # for i, packer in enumerate(evaluated_agent.env.packers):
-        #     packer.render().savefig(f"./outputs/gp_{step}_{i}.jpg")
         state = next_state
 
     return round(evaluated_agent.env.used_packers[0].space_utilization() * 100, 2)
@@ -29,13 +23,6 @@ def _evaluate_individual(agent, individual):
 def evaluate(population, agent):
     fitness_list = []
 
-    # with ThreadPoolExecutor(max_workers=1) as executor:
-    # futures = [
-    #     executor.submit(_evaluate_individual, agent, individual)
-    #     for individual in population
-    # ]
-
-    # fitness_list = [future.result() for future in futures]
     for individual in population:
         fitness = _evaluate_individual(agent, individual)
         fitness_list.append(fitness)
